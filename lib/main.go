6a245b056daebb61eaa4882042acf1dd06db2e9e
Cris Silva Jr <crisjr@pm.me>
2020-06-24 10:29:03 -0300

    Setup Golang WASM

---
+++ lib/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "syscall/js"
+)
+
+func add(this js.Value, i []js.Value) interface{} {
+    js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
+    return js.ValueOf(i[0].Int() + i[1].Int())
+}
+
+func subtract(this js.Value, i []js.Value) interface{} {
+    js.Global().Set("output", js.ValueOf(i[0].Int()-i[1].Int()))
+    return js.ValueOf(i[0].Int() - i[1].Int())
+}
+
+func registerCallbacks() {
+    js.Global().Set("add", js.FuncOf(add))
+    js.Global().Set("subtract", js.FuncOf(subtract))
+}
+
+func main() {
+    c := make(chan struct{}, 0)
+    fmt.Println("WASM Go Initialized")
+    registerCallbacks()
+    <-c
+}
