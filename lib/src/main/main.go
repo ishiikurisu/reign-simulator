3f76349a5eaf8c6d2058f5f4c27ba0430e0de128
Cris Silva Jr <crisjr@pm.me>
2020-07-03 23:17:29 -0300

    Start converting world to update society inside Go

--- lib/src/main/main.go
+++ lib/src/main/main.go
@@ -31,8 +31,47 @@ func loadMap(this js.Value, i []js.Value) interface{} {
     return ValueOf(gomapWorld)
 }
 
+func tick(this js.Value, i[]js.Value) interface{} {
+    // converting map from JS
+    mJs := i[0]
+    limitI := mJs.Length()
+    m := make([][]maps.Block, limitI)
+    for i := 0; i < limitI; i++ {
+        mi := mJs.Index(i)
+        limitJ := mi.Length()
+        m[i] = make([]maps.Block, limitJ)
+
+        for j := 0; j < limitJ; j++ {
+            mj := mi.Index(j)
+
+            kind := mj.Get("kind").String()
+
+            color := make([]uint32, 3)
+            colorJs := mj.Get("color")
+            for k := 0; k < 3; k++ {
+                color[k] = uint32(colorJs.Index(k).Int())
+            }
+
+            m[i][j] = maps.Block{
+                Kind: kind,
+                Color: color,
+            }
+        }
+    }
+    fmt.Printf("#%v\n", m)
+
+    // TODO convert society from JS
+    // sJs := i[1]
+    // s := make([]maps.Institution, 10)
+
+    // TODO Update society living on map
+    // return maps.Tick(m, s)
+    return "TODO"
+}
+
 func registerCallbacks() {
     js.Global().Set("loadMap", js.FuncOf(loadMap))
+    js.Global().Set("tick", js.FuncOf(tick))
 }
 
 func main() {
---
+++ lib/src/maps/society.go
@@ -0,0 +1,13 @@
+package maps
+
+type Institution struct {
+    What string
+    Where []int
+    // also needs a memory and a script to run
+}
+
+// Updates the society `s` living on the world `m`
+func Tick(m [][]Block, s []Institution) []Institution {
+    // TODO Implement me! Simulate society!
+    return nil
+}
